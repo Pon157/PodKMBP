82fe5ed2c7be302c47b7e4c80f821517bf2f8b8f
Pon157 <Vitechek.208@gmail.com>
2026-07-09 22:03:02 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
@@ -12,21 +12,10 @@ export default defineConfig(() => {
       },
     },
     server: {
-      hmr: process.env.DISABLE_HMR !== 'true',
-      watch: process.env.DISABLE_HMR === 'true' ? null : {},
-    },
-    // --- Добавляем секцию build ---
-    build: {
-      cssCodeSplit: false, // Запрещаем разделять CSS на разные файлы
-      rollupOptions: {
-        output: {
-          // Все JS-файлы из node_modules и компоненты пойдут в один index.js
-          manualChunks: undefined, 
-          entryFileNames: 'assets/index.js',
-          chunkFileNames: 'assets/index.js',
-          assetFileNames: 'assets/index.[ext]',
-        },
-      },
+      // HMR включен всегда
+      hmr: true,
+      // Наблюдение за файлами активно
+      watch: {},
     },
   };
 });
